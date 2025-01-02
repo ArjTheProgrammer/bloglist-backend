a9318c3849cd71da9314d078908bc84f33b98248
Jose Neil Silagan <neilsilagan@gmail.com>
2025-01-02 11:27:57 +0800

    Add logger util

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./app')
+const logger = require('./utils/logger')
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`)
+    logger.info(`server running on port ${PORT}`)
 })
