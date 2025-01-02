9a9e611bc12e021d0757a2bc14c0ac6ebebcf92a
Jose Neil Silagan <neilsilagan@gmail.com>
2025-01-02 12:20:26 +0800

    Add model for blog

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
 const app = express()
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to MongoDB')
+
+mongoose.connect(config.MONGODB_URI)
+.then(() => {
+    logger.info('connected to MongoDB')
+})
+.catch((error) => {
+    logger.error('error connecting to MongoDB: ', error.message)
+})
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
