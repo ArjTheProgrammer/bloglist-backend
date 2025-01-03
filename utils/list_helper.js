ee29c9067bd7279c5a3191714aa379ae567fed58
Jose Neil Silagan <neilsilagan@gmail.com>
2025-01-03 09:00:17 +0800

    Do exercises 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,14 @@
 const dummy = (blogs) => {
-    return Array.isArray(blogs) ? 1 : 0;
+    return Array.isArray(blogs) ? 1 : 0
   }
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((total, blog) => {
+        return total + blog.likes
+    }, 0)
+}
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
