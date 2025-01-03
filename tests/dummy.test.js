31e2f9eb920a304d1dd9cd7ffab2431b1b3359e9
Jose Neil Silagan <neilsilagan@gmail.com>
2025-01-03 08:43:48 +0800

    Do Exercises 4.3
    This is for the actual testing. Exercises 4.1 and 4.2 serves as a reviewer for the backend and also a practice for project structures

---
+++ tests/dummy.test.js
@@ -0,0 +1,19 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
+
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+  })
+
+  test('dummy returns 0', () => {
+    const blogs = 1
+  
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 0)
+  })
+})
