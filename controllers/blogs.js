78af8623ec4c4f0ff6d94afe8250e9162a03a0e7
Jose Neil Silagan <neilsilagan@gmail.com>
2025-03-24 09:47:32 +0800

    Finish Exercises 4.21

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -45,6 +45,19 @@ blogRouter.get('/:id', async (request, response) => {
 })
 
 blogRouter.delete('/:id', async (request, response) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+    if (!decodedToken.id){
+        response.status(401).json({error: 'token invalid'})
+    }
+
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+
+    if (user.id.toString() != blog.user.toString()){
+        response.status(403).json({error: "Invalid permission"}).end()
+    }
+
     await Blog.findByIdAndDelete(request.params.id)
 
     response.status(204).end()
